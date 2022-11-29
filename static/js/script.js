12c75c97d92d49f9942bfa8f13b57e312eeddbe1
Nur Hadi <decoding0506@gmail.com>
2022-11-29 16:24:02 +0700

    first commit

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// ========== Navbar Function
+window.addEventListener("scroll", function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+// ========== Toggle Menu
+function toggleMenu(){
+    const menuBar = document.querySelector(".menuToggle");
+    const nav = document.querySelector(".nav");
+    menuBar.classList.toggle('active');
+    nav.classList.toggle('active');
+}
